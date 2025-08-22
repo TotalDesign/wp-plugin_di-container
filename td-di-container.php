3fb9c57398678c66bc51f812689a6e002356decf
Robbert van 't Westende <255752+westende@users.noreply.github.com>
2025-08-22 13:45:31 +0200

    chore: Update League Container to 5.1.

--- td-di-container.php
+++ td-di-container.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name:  DI Container
 Description:  Dependency Injection Container for WordPress.
-Version:      0.1.0
+Version:      0.2.0
 Author:       Total Design
 Author URI:   https://www.totaldesign.com/
 License:      MIT License
 */
 
 global $container, $tdContainer;
-$tdContainer = $container = new League\Container\Container;
+$tdContainer = $container = new League\Container\Container();
 
 add_action('plugins_loaded', function () use ($container) {
     do_action('init_container', $container);
-}, -PHP_INT_MAX );
+}, -PHP_INT_MAX);
